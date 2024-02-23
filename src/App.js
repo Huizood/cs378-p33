7c650fb3db8c0c9c35bb8f723fd7ecec3157bdd0
hui01 <hf5567@utexas.edu>
2024-02-23 10:20:43 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
-
 import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+import ramen_image from './images/ramen.png';
+import gyoza_image from './images/gyoza.png';
+import logos from './images/IMG_8711.jpg';
+import katsu_image from './images/katsu-curry.png';
+import sushi from './images/sushi.png';
+import matcha from './images/matcha-cake.png';
+import mochii from './images/mochi.png';
+import yakitori from './images/yakitori.png';
+import t from './images/takoyaki.png';
+import s from './images/sashimi.png';
+import o from './images/okonomiyaki.png';
+import { useState } from 'react';
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
@@ -10,92 +21,159 @@ const menuItems = [
     id: 1,
     title: 'Gyoza',
     description: 'Japanese dumplings',
-    imageName: 'gyoza.png',
+    imageName: gyoza_image,
     price: 5.99,
   },
   {
     id: 2,
     title: 'Sushi',
     description: 'Japanese rice rolls',
-    imageName: 'sushi.png',
+    imageName: sushi,
     price: 6.99,
   },
   {
     id: 3,
     title: 'Ramen',
     description: 'Japanese noodle soup',
-    imageName: 'ramen.png',
+    imageName: ramen_image,
     price: 7.99,
   },
   {
     id: 4,
     title: 'Matcha Cake',
     description: 'Japanese green tea cake',
-    imageName: 'matcha-cake.png',
+    imageName: matcha,
     price: 4.99,
   },
   {
     id: 5,
     title: 'Mochi',
     description: 'Japanese rice cake',
-    imageName: 'mochi.png',
+    imageName: mochii,
     price: 3.99,
   },
   {
     id: 6,
     title: 'Yakitori',
     description: 'Japanese skewered chicken',
-    imageName: 'yakitori.png',
+    imageName: yakitori,
     price: 2.99,
   },
   {
     id: 7,
     title: 'Takoyaki',
     description: 'Japanese octopus balls',
-    imageName: 'takoyaki.png',
+    imageName: t,
     price: 5.99,
   },
   {
     id: 8,
     title: 'Sashimi',
     description: 'Japanese raw fish',
-    imageName: 'sashimi.png',
+    imageName: s,
     price: 8.99,
   },
   {
     id: 9,
     title: 'Okonomiyaki',
     description: 'Japanese savory pancake',
-    imageName: 'okonomiyaki.png',
+    imageName: o,
     price: 6.99,
   },
   {
     id: 10,
     title: 'Katsu Curry',
     description: 'Japanese curry with fried pork',
-    imageName: 'katsu-curry.png',
+    imageName: katsu_image,
     price: 9.99,
   }
 ];
 
 
+
 function App() {
+
+  const [itemCounts, setItemCounts] = useState(new Array(menuItems.length).fill(0));
+  const [total, setTotal] = useState(0);
+
+  const track = (price, cart) => {
+    if (cart === 'add') {
+      const adds = (total + price).toFixed(2);
+      setTotal(parseFloat(adds));
+    } else if (cart === 'sub') {
+      if (total > 0) {
+        const subtract = (total - price).toFixed(2);
+        setTotal(parseFloat(subtract));
+      }
+    }
+  };
+
+  const clearAll = () => {
+    setItemCounts(new Array(menuItems.length).fill(0));
+    setTotal(0);
+  };
+
+
+  const Order = () => {
+    const itemsInCart = itemCounts.reduce((acc, count, index) => {
+      if (count > 0) {
+        const menuItem = menuItems.find(item => item.id - 1 === index);
+        acc.push({ ...menuItem, quantity: count });
+      }
+      return acc;
+    }, []);
+
+    if (itemsInCart.length === 0) {
+      alert("No items in cart");
+    } else {
+      const orderSummary = itemsInCart
+        .map(item => `${item.title} - ${item.quantity}`)
+        .join("\n");
+      alert(`Order Placed:\n\n${orderSummary}`);
+    }
+  };
+
+
+
   return (
-      <div>
-        
-             <h1>
-                <img class="a" src="/images/IMG_8711.jpg" style={{ width: '50px', height: '50px' }} alt="logo" />
-                Japan Vibe Cafe
-            </h1>
-          <p id="slogan1">Delicious Authentic Japanese Food</p>
-          <p id="slogan2">Top1 Choice of Japanese Food at Austin!!!!</p>
-          <h1>Menu</h1>
-          <div className="menu">
-              {menuItems.map((i) => (
-                  <MenuItem title={i.title} description={i.description} price={i.price} imageName={i.imageName} />
-              ))}
-          </div>
+    <div>
+
+      <h1>
+        <img className="a" src={logos} style={{ width: '50px', height: '50px' }} alt="logo" />
+        Japan Vibe Cafe
+      </h1>
+      <p id="slogan1">Delicious Authentic Japanese Food</p>
+      <p id="slogan2">Top1 Choice of Japanese Food at Austin!!!!</p>
+      <h1>Menu</h1>
+      <div className="menu">
+        {menuItems.map((i, index) => (
+          <MenuItem title={i.title} description={i.description} price={i.price} imageName={i.imageName}
+            setItemCount={setItemCounts} track={track} index={i.id - 1} itemCounts={itemCounts[i.id - 1] || 0} />
+        ))}
       </div>
+
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3 subtotal">
+            <p className="subtotal">Subtotal ${total}</p>
+          </div>
+        </div>
+        <div className = "col-4 test">
+        <div className="button-container two">
+        <button onClick={Order} className="order">
+          <span className="orders">Order</span>
+        </button>
+        <button onClick={clearAll} className="clear">
+          <span className="clears">Clear all</span>
+        </button>
+      </div>
+        </div>
+        
+
+
+    </div>
+    </div >
   );
 }
 
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,36 +1,83 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState} from 'react';
 // This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title, description, price, imageName}) => {
+
+const MenuItem = ({ title, description, price, imageName, setItemCount , track , index, itemCounts}) => {
+
+    
+    const add_one = () => {
+        setItemCount(prevItemCounts => {
+          const newCounts = [...prevItemCounts];
+          newCounts[index]++; 
+          console.log("the counts", newCounts);
+          return newCounts;
+         
+        });
+        track(price, 'add');
+      };
+      
+      const sub_one = () => {
+        setItemCount(prevItemCounts => {
+          const newCounts = [...prevItemCounts];
+          if (newCounts[index] > 0) {
+            newCounts[index]--;
+            track(price, 'sub');
+          }
+          return newCounts;
+        });
+       
+      };
+    
+    
+
     return (
         <div>
-            <div className ="container">
+            <div className="container">
                 <div className="row">
                     <div className="col-4">
-                        <img src={'images/' + imageName} className = "b" alt={title} />
+                        <img src={imageName} className="b" alt={title} />
+
                     </div>
+
                     <div className="col-6">
-                        <b>{title}</b>
-                        <p>{description}</p>
                         <div className="row">
-                            <div className="col">
+                            <div className="col-5">
+                                <b>{title}</b>
+                                <p>{description}</p>
+                            </div>
+                            <div className="col-3">
                                 <p>${price}</p>
                             </div>
-                            <div className="col">
+                        </div>
+                        <div className="row">
+                            <div className="col-5"></div>
+                            <div className="col-sm-1">
+                                <button className="i" onClick={sub_one}><span className="minus">-</span></button>
                             </div>
-                            <div className="col">
-                                <button className="button">Add</button>
+                            <div className="col-sm-1">
+                                <p className= "counts">{itemCounts}</p>
+                            </div>
+                            <div className="col-sm-1">
+                                <button className="i" onClick={add_one}>+</button>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
+
         </div>
+
+
     );
 
 };
+
+
+
+
 export default MenuItem;
